4bc623e6cdb96f726cb278f648e7b145755389e2
qiusun0215 <53621589+qiusun0215@users.noreply.github.com>
2020-02-27 00:36:54 +0800

    Update parsingargs.h

--- parsingargs.h
+++ parsingargs.h
@@ -1,50 +1,45 @@
 #ifndef SBIF_PARSING_H
 #define SBIF_PARSING_H
 
-//according to https://blog.csdn.net/fly542/article/details/19486185
-/* 
- *   ParsingArgs pa;
- *   pa.AddArgType('l',"getlist", ParsingArgs::NO_VALUE); // There is no parameters
- *   pa.AddArgType('p',"getuser", ParsingArgs::MAYBE_VALUE); // There might be parameters
- *   pa.AddArgType('o',"outFile", ParsingArgs::MUST_VALUE); // There has to be parameters
- *   std::map<std::string, std::vector<std::string> > result;
- *   int iRet = pa.Parse(tmpPara,result); //
- *
- */
 #include <map>
 #include <vector>
 #include <string>
- 
+
+using namespace std;
+
+//according to https://blog.csdn.net/fly542/article/details/19486185
+
 class ParsingArgs
 {
 public:
     ParsingArgs();
     ~ParsingArgs();
-    
-    enum KeyFlag{ INVALID_KEY=-1, NO_VALUE, MAYBE_VALUE, MUST_VALUE};
 
-    bool AddArgType(const char shortName, const char * longName = NULL, KeyFlag flag=NO_VALUE);
-  
-    int Parse(const std::string & paras, std::map<std::string, std::vector<std::string> > & result, std::string &errPos);
- 
+    enum KeyType { INVALID = -1, NO, MAYBE, MUST };
+
+    bool AddArgType(const string shortName, const string longName = NULL, KeyType type = NO);
+
+    int Parse(const string& paras, map<string, vector<string> >& result, string& errPos);
+
 private:
-    
-    KeyFlag GetKeyFlag(std::string &key);
- 
-    void RemoveKeyFlag(std::string & paras);
- 
-    bool GetWord(std::string & Paras, std::string & word);
 
-    bool IsDuplicateKey(const std::string &key, const std::map<std::string, std::vector<std::string> > & result);
- 
+    KeyType GetKeyType(string& key);
+
+    void RemoveKeyType(string& paras);
+
+    bool GetWord(string& Paras, string& word);
+
+    bool IsDuplicateKey(const string& key, const map<string, vector<string> >& result);
+
     struct Option
     {
-        std::string m_longName;
-        char m_shortName;
-        KeyFlag m_flag;
+        string longName;
+        string shortName;
+        KeyType type;
     };
- 
-    std::vector<Option> m_args; 
+
+    vector<Option> args;
 };
- 
-#endif
+
+#endif  //SBIF_PARSING_H
+
