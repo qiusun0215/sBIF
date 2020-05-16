901067e73513646eccf7bd69430dff1bf6e19174
qiusun0215 <53621589+qiusun0215@users.noreply.github.com>
2020-05-16 22:27:06 +0800

    Create drosChr2L.sh

---
+++ example/drosChr2L.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##parameters
+interfile="../data/S2Rplus_spec_chr2L_11M_12M.txt"
+chrlensfile="../data/dm3_chrm_sizes"
+chrom="chr2L"
+start=11000000
+end=12000000
+outfolder="../output"
+res=2000
+job_prefix="dros"
+threads=20
+EXE_PATH="./../bin/sBIF"
+
+##command
+cmd="$EXE_PATH -i $interfile -c $chrom -l $chrlensfile -s $start -e $end -o $outfolder -r $res -j $job_prefix -p $threads "
+echo $cmd
+$cmd 
+echo "Done."
